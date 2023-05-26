6a069381b3e0eaaa25ac5c56d8c6fd15967f1b7c
Real-Chaos <rizwannafees13@gmail.com>
2023-05-25 22:10:06 -0400

    final

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+app.use("cors");
+app.use(exprss.static("build"));
 app.use(express.json());
 
 let contacts = [
